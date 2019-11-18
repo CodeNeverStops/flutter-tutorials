a5edfa151d1be92e3886c6c38a714d8fefe2b7c7
Wayne.You <lokiwuxi@gmail.com>
2019-11-18 16:44:37 +0800

    update

---
+++ 002-flutter-flare-pt1/01-start/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/youwei/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/youwei/projects/flutter/flutter-tutorials/002-flutter-flare-pt1/01-start"
+export "FLUTTER_TARGET=/Users/youwei/projects/flutter/flutter-tutorials/002-flutter-flare-pt1/01-start/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/youwei/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
